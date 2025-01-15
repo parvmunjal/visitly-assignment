b2b77ae1a9d305254a1578ef6c3f5b1ff6b987bc
parvmunjal <parvmunjal100@gmail.com>
2025-01-15 12:08:03 +0530

    Frontend config

--- src/main/java/com/visitly/assignment/controllers/AuthController.java
+++ src/main/java/com/visitly/assignment/controllers/AuthController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     @Autowired
