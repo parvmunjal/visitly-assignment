27cc58c1b15f3cad42c963fa051e94bfe5bcb02e
parvmunjal <parvmunjal100@gmail.com>
2025-01-15 10:06:34 +0530

    Procfile updated

--- src/main/java/com/visitly/assignment/AssignmentApplication.java
+++ src/main/java/com/visitly/assignment/AssignmentApplication.java
@@ -1,6 +1,5 @@
 package com.visitly.assignment;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
