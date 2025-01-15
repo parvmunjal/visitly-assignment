b8c28e45ff9d7048556cb1db43e4060d58ce8f59
parvmunjal <parvmunjal100@gmail.com>
2025-01-15 09:36:18 +0530

    Heroku Deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-java -jar target/assignment-0.0.1-SNAPSHOT.jar
+java -Dserver.port=$PORT -jar target/assignment-0.0.1-SNAPSHOT.jar
--- src/main/java/com/visitly/assignment/AssignmentApplication.java
+++ src/main/java/com/visitly/assignment/AssignmentApplication.java
@@ -1,5 +1,6 @@
 package com.visitly.assignment;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,13 @@ import org.springframework.context.annotation.Bean;
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// Check if the variables are being loaded
+		System.out.println(dotenv.get("SPRING_DATASOURCE_URL"));
+		System.out.println(dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.out.println(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
 		SpringApplication.run(AssignmentApplication.class, args);
 	}
 }
