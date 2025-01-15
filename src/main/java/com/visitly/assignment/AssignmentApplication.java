b3399ab525a7a02966995ec8495e480cd3c91cfb
parvmunjal <parvmunjal100@gmail.com>
2025-01-15 10:04:29 +0530

    Procfile updated

--- src/main/java/com/visitly/assignment/AssignmentApplication.java
+++ src/main/java/com/visitly/assignment/AssignmentApplication.java
@@ -10,12 +10,6 @@ import org.springframework.context.annotation.Bean;
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-
-		// Check if the variables are being loaded
-		System.out.println(dotenv.get("SPRING_DATASOURCE_URL"));
-		System.out.println(dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.out.println(dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
 		SpringApplication.run(AssignmentApplication.class, args);
 	}
